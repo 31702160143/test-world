62fe0a2efe22b9a8cbf7d99fd78920d31c31db8b
10.12 <a10.12@MacBook-Pro.local>
2020-02-20 17:52:43 +0800

    创建了dog类

---
+++ GitHub测试/GitHub测试/XWdog.h
@@ -0,0 +1,13 @@
+//
+//  XWdog.h
+//  GitHub测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XWdog : NSObject
+
+@end
---
+++ GitHub测试/GitHub测试/XWdog.m
@@ -0,0 +1,13 @@
+//
+//  XWdog.m
+//  GitHub测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import "XWdog.h"
+
+@implementation XWdog
+
+@end
