a98ac887b5fe7c65f81c687d845f9cdd58229c51
10.12 <a10.12@MacBook-Pro.local>
2020-02-20 17:43:11 +0800

    创建person类

---
+++ GitHub测试/GitHub测试/XWPerson.h
@@ -0,0 +1,13 @@
+//
+//  XWPerson.h
+//  GitHub测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XWPerson : NSObject
+
+@end
---
+++ GitHub测试/GitHub测试/XWPerson.m
@@ -0,0 +1,13 @@
+//
+//  XWPerson.m
+//  GitHub测试
+//
+//  Created by 10.12 on 2020/2/20.
+//  Copyright © 2020年 学文. All rights reserved.
+//
+
+#import "XWPerson.h"
+
+@implementation XWPerson
+
+@end
